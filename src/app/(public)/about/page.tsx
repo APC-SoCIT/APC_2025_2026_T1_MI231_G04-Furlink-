be874c365f9f25357493c09f17757e0acfbb035a
Atasha Frances Gayle Doria <doriaatashafrancesgayle@gmail.com>
2026-09-19 16:12:06 +0000

    style: updated the header and paragraph font color for the about page.

--- src/app/(public)/about/page.tsx
+++ src/app/(public)/about/page.tsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
           What is <span className="brand-highlight">furlink</span>?
         </h1>
         <p>
-          furlink offers a hassle-free experience for both customers and service providers. It enables grooming businesses to advertise their services while allowing pet owners to discover, schedule, and manage appointments in one place. It also provides an innovative AI-powered grooming preview tool, giving users a visual reference of potential pet haircut styles before booking — bridging the gap between customer expectations and actual grooming outcomes.
+          <i>furlink</i> offers a hassle-free experience for both customers and service providers. It enables grooming businesses to advertise their services while allowing pet owners to discover, schedule, and manage appointments in one place. It also provides an innovative AI-powered grooming preview tool, giving users a visual reference of potential pet haircut styles before booking — bridging the gap between customer expectations and actual grooming outcomes.
         </p>
       </div>
     </main>
